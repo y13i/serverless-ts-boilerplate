fa3e7d86ae4a3eb1df77f4a67297d1ea1541a8f3
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-05-08 20:00:32 +0900

    use apex.js fork

--- handlers/hello.ts
+++ handlers/hello.ts
@@ -1,26 +1,19 @@
 import {install} from "source-map-support";
 install();
 
-import {APIGatewayEvent, Context, Callback} from "aws-lambda";
+import * as λ from "@y13i/apex.js";
+import {APIGatewayEvent, Context} from "aws-lambda";
 
 import Greeter from "../lib/greeter";
 
-export default async function hello(event: APIGatewayEvent, context: Context, callback: Callback): Promise<void> {
-  try {
-    console.log({event, context});
+export default λ(async (event: APIGatewayEvent, _context: Context) => {
+  const params  = event.queryStringParameters || {};
+  const greeter = new Greeter(params.name);
+  const message = greeter.greet(params.yourName);
 
-    const params = event.queryStringParameters || {};
-
-    const greeter = new Greeter(params.name);
-    const message = greeter.greet(params.yourName);
-
-    callback(undefined, {
-      statusCode: 200,
-      headers:    {},
-      body:       JSON.stringify({message}),
-    });
-  } catch (ex) {
-    console.error(ex);
-    callback(ex);
-  }
-}
+  return {
+    statusCode: 200,
+    headers:    {},
+    body:       JSON.stringify({message}),
+  };
+});
