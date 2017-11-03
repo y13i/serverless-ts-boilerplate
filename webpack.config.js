93113dc4128cfe949dd67cda31c5d837f1ccf2ed
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-11-03 19:18:15 +0900

    make modules external

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ const slsw = require("serverless-webpack");
 const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  entry:   slsw.lib.entries,
-  target:  "node",
-  devtool: "source-map",
+  entry:     slsw.lib.entries,
+  target:    "node",
+  devtool:   "source-map",
+  externals: [nodeExternals()],
 
   module: {
     rules: [
@@ -27,7 +28,7 @@ module.exports = {
 
   output: {
     libraryTarget: "commonjs",
-    path:          path.join(__dirname, "dist"),
+    path:          path.join(__dirname, "dist", "lambda"),
     filename:      "[name].js",
   },
 };
