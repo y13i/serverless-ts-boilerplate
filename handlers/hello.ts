a57a6d34313506495d1850ca89a4388f72b6d251
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-11-06 16:07:06 +0900

    replace apex.js with dalamb

--- handlers/hello.ts
+++ handlers/hello.ts
@@ -1,10 +1,10 @@
 import "source-map-support/register";
-import * as λ from "@y13i/apex.js";
+import dalamb from "dalamb";
 import {APIGatewayEvent} from "aws-lambda";
 
 import Greeter from "../lib/greeter";
 
-export default λ(async (event: APIGatewayEvent) => {
+export default dalamb<APIGatewayEvent>(async event => {
   const params  = event.queryStringParameters || {};
   const greeter = new Greeter(params.name);
   const message = greeter.greet(params.yourName);
